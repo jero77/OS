a216368efbeab14c83ccf66dc7e1f6c51ddc6c71
jero77 <34156935+jero77@users.noreply.github.com>
2017-12-08 11:35:48 +0100

    Added FIFO Mechanism, maybe works?

--- Uebung3/aufgabe2_3.c
+++ Uebung3/aufgabe2_3.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>    //File control options
 
+
+#define MAX 50
+
+
+/*
+  Kommunikation über benannte Pipe. Name der Pipe wird als Kommandozeilen-
+  parameter übergeben.
+*/
 int main(int argc, char const *argv[]) {
 
-
-  if (mknod("/tmp/mypipe", S_IFIFO) == -1) {
-    printf("Error mknod()\n");
+  //Teste auf richtige Paramterzahl
+  if (argc != 2) {
+    printf("Aufruf mit Kommandozeilenparameter 'Pfad zur benannten Pipe'!\n");
     exit(EXIT_FAILURE);
   }
 
+  //Öffne Pipe (S_IFIFO) mit Lese- (S_IRUSR) und Schreibrechten (S_IWUSR)
+  //Letztes Argument von mknod wird bei Pipe ignoriert also 0
+  if (mknod(argv[1], S_IRUSR | S_IWUSR | S_IFIFO , 0) == -1) {
+    printf("Fehler bei mknod(), konnte Pipe nicht öffnen\n");
+    exit(EXIT_FAILURE);
+  }
+  //Filedeskriptor nur zum Lesen
+  int fdr = open(argv[1], O_RDONLY);
+  char buffer[MAX];
+  read(fdr, buffer, MAX);
+  printf("Gelesen:%s\n", buffer);
 
   return 0;
 }
