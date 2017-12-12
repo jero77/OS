9cc9c328d31e5efe383fbe4bc4fd8d9695dd0a70
jero77 <jeros.2808@gmx.de>
2017-12-12 13:39:14 +0100

    Added Client and Server files

---
+++ Uebung4/client.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/*
+  Clientprozess, der mit dem Serverprozess "server.c" kommuniziert.
+*/
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
---
+++ Uebung4/server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/*
+  Serverprozess, der mit Clients von "client.c" kommuniziert.
+*/
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
