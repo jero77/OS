65e5318896a06af36433ef4598f78e83a63fe5cb
jero77 <jeros.2808@gmx.de>
2017-12-12 12:24:46 +0100

    Added comments

--- Uebung4/aufgabe1_1.c
+++ Uebung4/aufgabe1_1.c
@@ -8,11 +8,17 @@
   Ausgabe hinein.
 */
 int main(int argc, char const *argv[]) {
+
+  //Öffne die Datei "test.txt"
   int fd = open("test.txt", O_WRONLY | O_CREAT, S_IRWXU);
 
+  //Schließe den Standardoutput-Filedeskriptor
   close(STDOUT_FILENO);
+
+  //Dupliziere den Filedeskriptor fd nach STDOUT_FILENO
   dup2(fd, STDOUT_FILENO);
 
+  //Schreibe auf die Standardausgabe (-> also in die Datei "test.txt")
   printf("Die Summe von 2 + 2 ist %d\n", 2+2);
 
   return 0;
