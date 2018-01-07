cee9cacf6ca7621caf2b375cf9f568777c4088b9
jero77 <34156935+jero77@users.noreply.github.com>
2018-01-07 14:14:34 +0100

    println sleep time

--- Uebung5/Java/SleepingOfficial.java
+++ Uebung5/Java/SleepingOfficial.java
@@ -16,6 +16,7 @@ public class SleepingOfficial {
       Request r = produceRequest();
       //Sleep a random amount of time 1s - 5s
       int s = (int) (Math.random() * 5) + 1;
+      System.out.println(s);
       Thread.sleep(s);
       Applicant app = new Applicant(r, monitor);
       app.start();
